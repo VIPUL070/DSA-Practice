619753baf05d38228f763cb45636d270d0a4ef46
VIPUL070 <sutharvipul007@gmail.com>
2026-04-27 22:39:44 +0530

    Solving sort colors problem

---
+++ pattern/RUNNING_ARRAY/Sort_Colors.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+void sortColors(vector<int> &nums)
+{
+    int s = 0;
+    int mid = 0;
+    int e = nums.size() - 1;
+
+    while (mid <= e)
+    {
+        if (nums[mid] == 0)
+        {
+            swap(nums[mid], nums[s]);
+            mid++;
+            s++;
+        }
+        else if (nums[mid] == 1)
+        {
+            mid++;
+        }
+        else
+        {
+            swap(nums[mid], nums[e]);
+            e--;
+        }
+    }
+}
